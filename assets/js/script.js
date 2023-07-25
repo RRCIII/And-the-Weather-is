8e1bc80827d0634bdc0a1aef99b4a003662a163b
Ricky Carter <randolphcarter5@gmail.com>
2023-07-25 13:10:45 -0400

    updated JS w/ variables defined using vanilla js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 var APIkey = "5c1d4eb10a710338c05be89784ee93af";
-var cityName = "https://api.openweathermap.org/data/2.5/weather?q={city name}&units=imperial&appid={APIkey}";
+var cityName = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${APIkey}`;
+
+//getElementsById and querySelector 
+const searchform = document.querySelector('.search-form');
+const citName = document.getElementById('city-name');
+const searchBtn = document.getElementById('search-btn');
+const section = document.querySelector('section');
+const nextFiveDays = document.querySelector('.next-five-days');
+const searchedCities = document.querySelector('.cities-searched');
